89c18b793e92193f8d8bfa3848930a1a2cb6b90e
BOUKHLIF <boukhlifmohamed13@gmail.com>
2017-11-30 00:29:02 +0000

    starter config and pages

--- src/pages/register/register.ts
+++ src/pages/register/register.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the RegisterPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { User } from '../../models/user';
 
 @IonicPage()
 @Component({
@@ -15,11 +9,13 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class RegisterPage {
 
+  user = {} as User
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
+  
+  register() {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad RegisterPage');
   }
 
 }
