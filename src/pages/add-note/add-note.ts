e91baf16bc1a86bf032f30b07350db1e4840b3c6
BOUKHLIF Mohamed <boukhlifmohamed13@gmail.com>
2018-02-21 11:31:09 +0000

    add note form validation

--- src/pages/add-note/add-note.ts
+++ src/pages/add-note/add-note.ts
@@ -4,6 +4,7 @@ import { Component } from '@angular/core';
 import { DataProvider } from '../../providers/data/data';
 import { IonicPage } from 'ionic-angular';
 import { ToastProvider } from '../../providers/toast/toast';
+import { Validators, FormBuilder } from '@angular/forms';
 
 @IonicPage()
 @Component({
@@ -11,19 +12,33 @@ import { ToastProvider } from '../../providers/toast/toast';
   templateUrl: 'add-note.html'
 })
 export class AddNotePage {
-
+  addNote;
+  valid: boolean;
   note = {} as Note;
 
   constructor(
     public viewCtrl: ViewController,
     private dataProvider: DataProvider,
     private toastProvider: ToastProvider,
+    private formBuilder: FormBuilder,
   ) {
+    this.valid = true;
+    this.addNote = formBuilder.group({
+      generalIdea: ['', Validators.compose([Validators.required])],
+      summary: ['', Validators.compose([Validators.required])],
+      reminder: ['', Validators.compose([Validators.required]),]
+    });
+
     console.log('Hello AddNotePage');
     this.note.mid = this.viewCtrl.getNavParams().get('mid');
   }
   
   create(note: Note) {
+    if(!this.addNote.valid) {
+      this.valid = false;
+      return;
+    }
+
     console.log(note);
     note.date = (new Date).getTime();
     this.dataProvider.postNote(note).then(() => {
